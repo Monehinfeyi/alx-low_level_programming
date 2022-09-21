56b65c285db6195ac94d56585b4fd2dac7c53db2
Monehin <monehinfeyisara.d@gmail.com>
2022-09-21 15:13:26 -0700

    My prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -10,7 +12,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char *rot13(char *s)
+char *rot13(char *s);
 void print_number(int n);
 
 #endif /* MAIN_H */
