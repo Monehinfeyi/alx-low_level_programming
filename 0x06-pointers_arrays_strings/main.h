df64c42ccbe779dcf257c74586820b2f30e0f563
Monehin <monehinfeyisara.d@gmail.com>
2022-09-22 04:38:50 -0700

    Prototypes

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *s);
 void print_number(int n);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif /* MAIN_H */
