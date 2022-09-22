b39c641e5cb0f4cd54f42e1967e8c2bd03a913a4
Monehin <monehinfeyisara.d@gmail.com>
2022-09-22 04:30:29 -0700

    Addiction happens

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,14 +4,15 @@ char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 /**
- * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
- * @r: The buffer to store the result.
- * @r_index: The current index of the buffer.
- * Return: If r can store the sum - a pointer to the result.
- * If r cannot store the sum - 0.
- */
+* add_strings - Adds the numbers stored in two strings.
+* @n1: The string containing the first number to be added.
+* @n2: The string containing the second number to be added.
+* @r: The buffer to store the result.
+* @r_index: The current index of the buffer.
+*
+* Return: If r can store the sum - a pointer to the result.
+* If r cannot store the sum - 0.
+*/
 
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
@@ -38,11 +39,13 @@ num = (*n2 - '0') + tens;
 *(r + r_index) = (num % 10) + '0';
 tens = num / 10;
 }
+
 if (tens && r_index >= 0)
 {
 *(r + r_index) = (tens % 10) + '0';
 return (r + r_index);
 }
+
 else if (tens && r_index < 0)
 return (0);
 
@@ -50,14 +53,15 @@ return (r + r_index + 1);
 }
 
 /**
- * infinite_add - Adds two numbers.
- * @n1: The first number to be added.
- * @n2: The second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
- * Return: If r can store the sum - a pointer to the result.
- * If r cannot store the sum - 0.
- */
+* infinite_add - Adds two numbers.
+* @n1: The first number to be added.
+* @n2: The second number to be added.
+* @r: The buffer to store the result.
+* @size_r: The buffer size.
+*
+* Return: If r can store the sum - a pointer to the result.
+* If r cannot store the sum - 0.
+*/
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
