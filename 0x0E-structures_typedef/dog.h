5a75eee123c07583d39f14b62d26e815b2bc21a6
Monehin <monehinfeyisara.d@gmail.com>
2022-10-10 09:59:11 -0700

    The main prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,6 +5,25 @@
 #include <stddef.h>
 #include <stdlid.h>
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
+/**
+ * struct dog - structure for dogs
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+char *nam;
+float age;
+char *owner;
+};
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
