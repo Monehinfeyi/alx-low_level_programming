fe52c36503c9f337788d4458e72ec7946e808e1e
Monehin <monehinfeyisara.d@gmail.com>
2022-10-12 10:16:24 -0700

    Correction happened

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,6 +5,7 @@
  * main - prints the opcodes of its own main function
  * @argc: argument count
  * @argv: argument vector
+ *
  * Return: The job has been finished
  */
 
@@ -15,6 +16,7 @@ if (argc != 2)
 printf("Error\n");
 exit(1);
 }
+
 if (atoi(argv[1]) < 0)
 {
 printf("Error\n");
